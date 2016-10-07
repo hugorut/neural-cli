6ea0b03cdb20b0962eb8f0078bea12e98932ccb1
Hugo Rut <hugorut@gmail.com>
2016-10-07 19:49:27 +0100

    change package naming convention

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'neural-cli',
-  packages = ['neural-cli'],
-  version = '0.4',
+  name = 'neuralcli',
+  packages = ['neuralcli'],
+  entry_points = {
+      "console_scripts": ['neuralcli = neuralcli.cli:main']
+      },
+  version = '0.5',
   description = 'A command line neural network',
   author = 'Hugo Rut',
   author_email = 'hugorut@gmail.com',
   url = 'https://github.com/hugorut/neural-cli',
-  download_url = 'https://github.com/hugorut/neural-cli/tarball/0.4', 
+  download_url = 'https://github.com/hugorut/neural-cli/tarball/0.5', 
   keywords = ['machine learning', 'ai', 'neural nework'], 
   classifiers = [],
 )
